54d32556474cbbea030e48bf448b974c6e594c88
bpenovic <bozo.penovic@lilcodelab.com>
2020-09-22 22:59:47 +0200

    Added markers and polylines on map

--- AngularOSM/src/app/app.component.ts
+++ AngularOSM/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { latLng, tileLayer } from 'leaflet';
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,50 @@ import { latLng, tileLayer } from 'leaflet';
 })
 export class AppComponent {
   title = 'AngularOSM';
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors'
-      })
-    ],
-    zoom: 7,
-    center: latLng([ 46.879966, -121.726909 ])
+
+  options: Leaflet.MapOptions = {
+    layers: getLayers(),
+    zoom: 12,
+    center: new Leaflet.LatLng(43.530147, 16.488932)
   };
 }
+
+export const getMarkers = (): Leaflet.Marker[] => {
+  return [
+    new Leaflet.Marker(new Leaflet.LatLng(43.5121264, 16.4700729), {
+      icon: new Leaflet.Icon({
+        iconSize: [50, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/blue-marker.svg',
+      }),
+      title: 'Workspace'
+    } as Leaflet.MarkerOptions),
+    new Leaflet.Marker(new Leaflet.LatLng(43.5074826, 16.4390046), {
+      icon: new Leaflet.Icon({
+        iconSize: [50, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/red-marker.svg',
+      }),
+      title: 'Riva'
+    } as Leaflet.MarkerOptions),
+  ] as Leaflet.Marker[];
+};
+
+export const getRoutes = (): Leaflet.Polyline[] => {
+  return [
+    new Leaflet.Polyline([
+      new Leaflet.LatLng(43.5121264, 16.4700729),
+      new Leaflet.LatLng(43.5074826, 16.4390046),
+    ] as Leaflet.LatLng[])
+  ] as Leaflet.Polyline[];
+};
+
+export const getLayers = (): Leaflet.Layer[] => {
+  return [
+    new Leaflet.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    } as Leaflet.TileLayerOptions),
+    ...getMarkers(),
+    ...getRoutes()
+  ] as Leaflet.Layer[];
+};
